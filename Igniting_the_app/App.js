87533d49629203ac1e405c90f6d2e0f1fa2059f2
rahesh <selvakumarrahesh@gmail.com>
2024-05-09 17:18:05 +0530

    Adding the 2nd Episode file changes

---
+++ Igniting_the_app/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+//const heading = React.createElement("h1", { id: "heading" }, "Hello world from React");
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" }, [React.createElement("h1", { id: "heading" }, "This is the header tag"), React.createElement("h2", { id: "heading2" }, "This is the header2 tag")])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
